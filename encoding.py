55c140ac6244341e994c425d9c1b013a8d9bca69
jiguotong <1776220977@qq.com>
2023-08-09 10:55:42 +0800

    [Update]更改目录结构，修改代码

--- encoding.py
+++ encoding.py
@@ -1,17 +1,20 @@
+'''
+author:jiguotong
+time:2023/08/08
+description:对数据库所有图片进行检测，对齐，编码
+note:在更换网络结构(backbone)后一定要重新进行人脸编码，运行encoding.py。
+'''
+
 import os
+from recognition import FaceRecognition
 
-from retinaface import Retinaface
-
-'''
-在更换facenet网络后一定要重新进行人脸编码，运行encoding.py。
-'''
-retinaface = Retinaface(1)
-
-list_dir = os.listdir("face_dataset")
+face_Recog = FaceRecognition(encoding=1)
+database_dir = 'data/database'
+list_dir = os.listdir(database_dir)
 image_paths = []
 names = []
 for name in list_dir:
-    image_paths.append("face_dataset/"+name)
+    image_paths.append(os.path.join(database_dir, name))
     names.append(name.split("_")[0])
 
-retinaface.encode_face_dataset(image_paths,names)
+face_Recog.encode_face_dataset(image_paths,names)
--- predict.py
+++ predict.py
@@ -4,14 +4,13 @@
 #   整合到了一个py文件中，通过指定mode进行模式的修改。
 #----------------------------------------------------#
 import time
-
 import cv2
 import numpy as np
 
-from retinaface import Retinaface
+from recognition import FaceRecognition
 
 if __name__ == "__main__":
-    retinaface = Retinaface()
+    retinaface = FaceRecognition()      # 构建识别示例
     #----------------------------------------------------------------------------------------------------------#
     #   mode用于指定测试的模式：
     #   'predict'表示单张图片预测，如果想对预测过程进行修改，如保存图片，截取对象等，可以先看下方详细的注释
@@ -19,7 +18,7 @@ if __name__ == "__main__":
     #   'fps'表示测试fps，使用的图片是img里面的street.jpg，详情查看下方注释。
     #   'dir_predict'表示遍历文件夹进行检测并保存。默认遍历img文件夹，保存img_out文件夹，详情查看下方注释。
     #----------------------------------------------------------------------------------------------------------#
-    mode = "predict"
+    mode = "dir_predict"
     #----------------------------------------------------------------------------------------------------------#
     #   video_path用于指定视频的路径，当video_path=0时表示检测摄像头
     #   想要检测视频，则设置如video_path = "xxx.mp4"即可，代表读取出根目录下的xxx.mp4文件。
@@ -42,8 +41,8 @@ if __name__ == "__main__":
     #   dir_save_path指定了检测完图片的保存路径
     #   dir_origin_path和dir_save_path仅在mode='dir_predict'时有效
     #-------------------------------------------------------------------------#
-    dir_origin_path = "img/"
-    dir_save_path   = "img_out/"
+    dir_origin_path = "data/input_image/"
+    dir_save_path   = "data/output_image/"
 
     if mode == "predict":
         '''
@@ -64,6 +63,7 @@ if __name__ == "__main__":
                 image   = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
                 r_image = retinaface.detect_image(image)
                 r_image = cv2.cvtColor(r_image,cv2.COLOR_RGB2BGR)
+                cv2.imwrite(dir_save_path+'output.jpg',r_image)
                 cv2.imshow("after",r_image)
                 cv2.waitKey(0)
 
@@ -112,10 +112,11 @@ if __name__ == "__main__":
         cv2.destroyAllWindows()
 
     elif mode == "fps":
-        img = cv2.imread('img/obama.jpg')
+        img = cv2.imread('{}/obama.jpg'.format(dir_origin_path))
         img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
         tact_time = retinaface.get_FPS(img, test_interval)
         print(str(tact_time) + ' seconds, ' + str(1/tact_time) + 'FPS, @batch_size 1')
+
     elif mode == "dir_predict":
         import os
 
@@ -132,5 +133,6 @@ if __name__ == "__main__":
                 if not os.path.exists(dir_save_path):
                     os.makedirs(dir_save_path)
                 cv2.imwrite(os.path.join(dir_save_path, img_name), r_image)
+    
     else:
         raise AssertionError("Please specify the correct mode: 'predict', 'video', 'fps' or 'dir_predict'.")
